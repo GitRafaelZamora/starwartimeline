212615d5809b32cd0ba71130c1811f575aaa9b14
Rafael Zamora <corellia@Rafaels-MacBook-Pro.local>
2018-07-01 22:46:28 -0700

    attempt at fixing CORS with jQuery request

--- js/app.js
+++ js/app.js
@@ -11,8 +11,19 @@ new Vue({
     },
     methods: {
         getMovies: function() {
-            this.$http.get(apiURL, function(movies) {
-                this.$set('movies', movies);
+
+            var settings = {
+              "async": true,
+              "crossDomain": true,
+              "url": "http://dev-starwarstimeline.pantheonsite.io/api/movies",
+              "method": "GET",
+              "headers": {
+                "cache-control": "no-cache",
+              }
+            }
+            let that = this;
+            $.ajax(settings).done(function (movies) {
+                that.$set('movies', movies);
                 console.log(movies);
             });
         }
