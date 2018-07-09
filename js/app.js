a102b5cc85644b11f76f2c6837e2a69915bdc496
Rafael Zamora <corellia@Rafaels-MacBook-Pro.local>
2018-07-08 23:30:30 -0700

    Simplified the API interface a bit

--- js/app.js
+++ js/app.js
@@ -1,31 +1,26 @@
-apiURL = 'http://dev-starwarstimeline.pantheonsite.io/api/movies';
+apiURL = 'http://localhost:8888/moviesapp/api/movies';
 
 new Vue({
     el: '#app', 
     data: {
-        hello: 'Hello World Again!',
         movies: ''
     },
     ready: function() {
-        this.getMovies();
+      this.getMovies();
     },
     methods: {
         getMovies: function() {
-
-            var settings = {
-              "async": true,
-              "crossDomain": true,
-              "url": "http://dev-starwarstimeline.pantheonsite.io/api/movies",
-              "method": "GET",
-              "headers": {
-                "cache-control": "no-cache",
-              }
-            }
-            let that = this;
-            $.ajax(settings).done(function (movies) {
-                that.$set('movies', movies);
-                console.log(movies);
-            });
-        }
+          this.$http.get(apiURL, function(movies) {
+            console.log(movies[1].url);
+            this.$set('movies', movies.slice().sort(function (a, b) {
+              var first = a.order;
+              var second = b.order;
+                if (first < second) return -1;
+                if (first  > second) return 1;
+                return 0;
+              })
+            );
+          });
+        },
     }
 })
